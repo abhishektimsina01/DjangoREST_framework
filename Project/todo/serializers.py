43e9f6fc8c3b538038764f52fd87969e3d5cc62f
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-02-04 16:33:54 +0545

    Serialization

--- Project/todo/serializers.py
+++ Project/todo/serializers.py
@@ -6,7 +6,3 @@ class TodoSerializer(serializers.ModelSerializer):
         model=Todos
         fields = "__all__"
 
-    # def validate_todo(self, data):
-    #     if len(data['todo']) >=10:
-    #         raise serializers.ValidationError('Too long for topic')
-    #     return 
--- Project/todo/views.py
+++ Project/todo/views.py
@@ -13,28 +13,35 @@ def getAllTodos(request):
     serialized = TodoSerializer(todos, many = True)
     return Response(data = serialized.data)
 
+
 @api_view(['DELETE'])
 def deleteAll(request, id):
     try:
         res = Todos.objects.get(id = id)
     except:
-        return Response({'error' : 'element not found'})
+        return Response({'error' : 'issue'})
     Todos.objects.get(id = id).delete()
     return Response({"message" : 'removed the todo list'})
+
 
 @api_view(['POST'])
 def add_todo(request):
     print(request.POST)
     serializer = TodoSerializer(data = request.data)
     if serializer.is_valid():
+        print(serializer)
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors)
 
+
 @api_view(['PUT'])
 def updateTodo(request, id):
-    print(request)
-    todo = Todos.objects.get(id = id)
+    try: 
+        todo = Todos.objects.get(id = id)
+    except:
+        return Response({'error' : 'The task does not exist'})
+    print(todo)
     serializer = TodoSerializer(
         todo,
         data = request.data,
